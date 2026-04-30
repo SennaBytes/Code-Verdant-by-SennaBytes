483076ebc3c99a024b074af55e706816087745d5
Senna Klijn <103580@glr.nl>
2026-04-30 18:11:42 +0000

    DIRECTORIES

---
+++ School_Project/Portfolio_April26/js/script.js
@@ -0,0 +1,62 @@
+/* ===================================
+   Project: [Portfolio | Senna Klijn]
+   File: script.js
+   Author: [Senna Klijn]
+   Description: Main script
+   =================================== */
+
+/* ==========================
+       content styling  
+ ========================== */
+
+// Variables
+const bg = document.querySelector(".bg");
+const mb = document.querySelector(".mb");
+const mm = document.querySelector(".mm");
+const mf = document.querySelector(".mf");
+const fr = document.querySelector(".fr");
+
+// World size - scroll smoothness
+const world = document.querySelector(".bg");
+const maxScroll = world.scrollWidth - window.innerWidth;
+
+const ease = 0.08;
+let target = 0;
+let current = 0;
+
+
+
+// Scrolling Input fast-function
+window.addEventListener("wheel", (e) => {
+   e.preventDefault();
+   target += (e.deltaY || e.deltaX) * 1;
+   target = Math.max(0, Math.min(target, maxScroll));
+   current = Math.max(0, Math.min(current, maxScroll));
+
+}, { passive: false });
+
+// Scrolling Animation function
+function scrollAni() {
+
+   current += (target - current) * ease;
+
+   bg.style.transform = `translateX(${-current * 0.2}px)`;
+   mb.style.transform = `translateX(${-current * 0.4}px)`;
+   mm.style.transform = `translateX(${-current * 0.6}px)`;
+   mf.style.transform = `translateX(${-current * 0.8}px)`;
+   fr.style.transform = `translateX(${-current * 1}px)`;
+
+   requestAnimationFrame(scrollAni);
+}
+
+scrollAni();
+
+
+
+
+
+
+
+
+
+   
